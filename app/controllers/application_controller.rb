c8be8dbffbc75fbdbbc9e01b503e53fb8ac1a83e
Scott Warren <scott_warren@flightcentre.com>
2015-02-14 14:08:39 +1000

    :new_moon_with_face: fixed product->tenant association

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   around_filter :scope_current_tenant
 
 private
-  
+
   def current_tenant
     Tenant.find_by_domain! request.domain
   end
   helper_method :current_tenant
-  
+
+  def current_product
+    Product.find_or_create_by(:tenant_id => current_tenant.id)
+  end
+  helper_method :current_product
+
   def scope_current_tenant
     Tenant.current_id = current_tenant.id
     yield
   ensure
     Tenant.current_id = nil
   end
-  
 end
