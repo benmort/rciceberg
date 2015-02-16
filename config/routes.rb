7cc7dece20f735f446c9b1920f6ffcb3801c1711
Scott Warren <scott_warren@flightcentre.com>
2015-02-16 20:45:35 +1000

    :lipstick: updated URLs to remove preorder

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Selfstarter::Application.routes.draw do
   resources :tenants
 
   root :to => 'preorder#index'
-  match '/preorder'               => 'preorder#index', :via => [:get,:post]
-  get 'preorder/checkout'
-  match '/preorder/share/:uuid'   => 'preorder#share', :via => :get
-  match '/preorder/ipn'           => 'preorder#ipn', :via => :post
-  match '/preorder/prefill'       => 'preorder#prefill', :via => [:get,:post]
-  match '/preorder/postfill'      => 'preorder#postfill', :via => [:get,:post]
+  match '/order'               => 'preorder#index', :via => [:get,:post]
+  get 'order/checkout'         => 'preorder#checkout', :via => :get
+  match '/order/share/:uuid'   => 'preorder#share', :via => :get
+  match '/order/ipn'           => 'preorder#ipn', :via => :post
+  match '/order/prefill'       => 'preorder#prefill', :via => [:get,:post]
+  match '/order/postfill'      => 'preorder#postfill', :via => [:get,:post]
 end
