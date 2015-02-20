b73da8bb2d2afd51390b3618d5635ebaa68f1e0d
Ben Mort <benjaminmort@gmail.com>
2015-02-21 09:49:22 +1000

    develop - removed images

--- app/models/image.rb
+++
@@ -1,4 +0,0 @@
-class Image < ActiveRecord::Base
-	attr_accessible :source
-	belongs_to :imageable, polymorphic: true
-end
--- db/migrate/20150208094158_create_image.rb
+++
@@ -1,8 +0,0 @@
-class CreateImage < ActiveRecord::Migration
-  def change
-    create_table :images do |t|
-    	t.attachment :source
-    	t.references :imageable, polymorphic: true, index: true
-    end
-  end
-end
