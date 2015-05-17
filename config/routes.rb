18b7623b0a2d1ff1e683c1eb66aaf241394b12c7
Scott Warren <scott_warren@flightcentre.com>
2015-05-17 22:44:15 +1000

    added confirmation route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Selfstarter::Application.routes.draw do
 
   root :to => 'pages#landing_page'
   match '/order'               => 'order#index', :via => [:get,:post]
-  match 'checkout'             => 'order#checkout', :via => :get
-  match 'order/subscribe'      => 'order#subscribe', :via => :post
+  match '/checkout'            => 'order#checkout', :via => :get
+  match '/subscribe'           => 'order#subscribe', :via => :post
+  match '/confirmation'        => 'order#confirmation' :via => [:get]
   match '/order/share/:uuid'   => 'order#share', :via => :get
   match '/order/ipn'           => 'order#ipn', :via => :post
   match '/order/prefill'       => 'order#prefill', :via => [:get,:post]
