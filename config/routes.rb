6de55e3bebf1a26527635f415035f6418da1bc22
Scott Warren <scott_warren@flightcentre.com>
2015-05-20 19:38:49 +1000

    fixed bug with routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Selfstarter::Application.routes.draw do
   match '/order'               => 'order#index', :via => [:get,:post]
   match '/checkout'            => 'order#checkout', :via => :get
   match '/subscribe'           => 'order#subscribe', :via => :post
-  match '/confirmation'        => 'order#confirmation' :via => [:get]
+  match '/confirmation'        => 'order#confirmation', :via => [:get]
   match '/order/share/:uuid'   => 'order#share', :via => :get
   match '/order/ipn'           => 'order#ipn', :via => :post
   match '/order/prefill'       => 'order#prefill', :via => [:get,:post]
